90064e8274573f19c3a325ae5e29a8b6228bb9cc
Nicholas-Westby <nicholas-westby@users.noreply.github.com>
2026-05-29 08:49:20 -0700

    fix: bootstrap dotnet from nix

--- flake.nix
+++ flake.nix
@@ -28,8 +28,8 @@
 
             DOTNET_CLI_TELEMETRY_OPTOUT = "1";
             DOTNET_NOLOGO = "1";
+            DOTNET_ROOT = "${pkgs.dotnet-sdk_10}/share/dotnet";
           };
         });
     };
 }
-
